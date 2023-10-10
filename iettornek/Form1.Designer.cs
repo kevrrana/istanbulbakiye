73e4a49d36a18229770d4919068c74f47bebcd5c
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-10 16:34:32 +0300

    Add files via upload

---
+++ iettornek/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace iettornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkİlkgiris = new System.Windows.Forms.RadioButton();
+            this.chk1Aktarma = new System.Windows.Forms.RadioButton();
+            this.chk2Aktarma = new System.Windows.Forms.RadioButton();
+            this.chk3Aktarma = new System.Windows.Forms.RadioButton();
+            this.chk4Aktarma = new System.Windows.Forms.RadioButton();
+            this.chkOgrenci = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOdeme = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn50 = new System.Windows.Forms.Button();
+            this.btm100 = new System.Windows.Forms.Button();
+            this.btn200 = new System.Windows.Forms.Button();
+            this.btn500 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkOgrenci);
+            this.groupBox1.Controls.Add(this.chk4Aktarma);
+            this.groupBox1.Controls.Add(this.chk3Aktarma);
+            this.groupBox1.Controls.Add(this.chk2Aktarma);
+            this.groupBox1.Controls.Add(this.chk1Aktarma);
+            this.groupBox1.Controls.Add(this.chkİlkgiris);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(142, 274);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aktarmalar";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn500);
+            this.groupBox2.Controls.Add(this.btn20);
+            this.groupBox2.Controls.Add(this.btn200);
+            this.groupBox2.Controls.Add(this.btn50);
+            this.groupBox2.Controls.Add(this.btm100);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(179, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(142, 274);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bakiye Yükleme";
+            // 
+            // chkİlkgiris
+            // 
+            this.chkİlkgiris.AutoSize = true;
+            this.chkİlkgiris.Location = new System.Drawing.Point(6, 36);
+            this.chkİlkgiris.Name = "chkİlkgiris";
+            this.chkİlkgiris.Size = new System.Drawing.Size(77, 20);
+            this.chkİlkgiris.TabIndex = 0;
+            this.chkİlkgiris.TabStop = true;
+            this.chkİlkgiris.Text = "İlk Giriş";
+            this.chkİlkgiris.UseVisualStyleBackColor = true;
+            // 
+            // chk1Aktarma
+            // 
+            this.chk1Aktarma.AutoSize = true;
+            this.chk1Aktarma.Location = new System.Drawing.Point(6, 62);
+            this.chk1Aktarma.Name = "chk1Aktarma";
+            this.chk1Aktarma.Size = new System.Drawing.Size(98, 20);
+            this.chk1Aktarma.TabIndex = 1;
+            this.chk1Aktarma.TabStop = true;
+            this.chk1Aktarma.Text = "1. Aktarma";
+            this.chk1Aktarma.UseVisualStyleBackColor = true;
+            // 
+            // chk2Aktarma
+            // 
+            this.chk2Aktarma.AutoSize = true;
+            this.chk2Aktarma.Location = new System.Drawing.Point(6, 88);
+            this.chk2Aktarma.Name = "chk2Aktarma";
+            this.chk2Aktarma.Size = new System.Drawing.Size(98, 20);
+            this.chk2Aktarma.TabIndex = 2;
+            this.chk2Aktarma.TabStop = true;
+            this.chk2Aktarma.Text = "2. Aktarma";
+            this.chk2Aktarma.UseVisualStyleBackColor = true;
+            // 
+            // chk3Aktarma
+            // 
+            this.chk3Aktarma.AutoSize = true;
+            this.chk3Aktarma.Location = new System.Drawing.Point(6, 114);
+            this.chk3Aktarma.Name = "chk3Aktarma";
+            this.chk3Aktarma.Size = new System.Drawing.Size(98, 20);
+            this.chk3Aktarma.TabIndex = 3;
+            this.chk3Aktarma.TabStop = true;
+            this.chk3Aktarma.Text = "3. Aktarma";
+            this.chk3Aktarma.UseVisualStyleBackColor = true;
+            // 
+            // chk4Aktarma
+            // 
+            this.chk4Aktarma.AutoSize = true;
+            this.chk4Aktarma.Location = new System.Drawing.Point(6, 140);
+            this.chk4Aktarma.Name = "chk4Aktarma";
+            this.chk4Aktarma.Size = new System.Drawing.Size(98, 20);
+            this.chk4Aktarma.TabIndex = 4;
+            this.chk4Aktarma.TabStop = true;
+            this.chk4Aktarma.Text = "4. Aktarma";
+            this.chk4Aktarma.UseVisualStyleBackColor = true;
+            // 
+            // chkOgrenci
+            // 
+            this.chkOgrenci.AutoSize = true;
+            this.chkOgrenci.Location = new System.Drawing.Point(6, 166);
+            this.chkOgrenci.Name = "chkOgrenci";
+            this.chkOgrenci.Size = new System.Drawing.Size(80, 20);
+            this.chkOgrenci.TabIndex = 5;
+            this.chkOgrenci.Text = "Öğrenci";
+            this.chkOgrenci.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(6, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "200";
+            // 
+            // btnOdeme
+            // 
+            this.btnOdeme.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOdeme.Location = new System.Drawing.Point(163, 11);
+            this.btnOdeme.Name = "btnOdeme";
+            this.btnOdeme.Size = new System.Drawing.Size(136, 45);
+            this.btnOdeme.TabIndex = 4;
+            this.btnOdeme.Text = "ÖDEME";
+            this.btnOdeme.UseVisualStyleBackColor = false;
+            // 
+            // btn20
+            // 
+            this.btn20.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btn20.Location = new System.Drawing.Point(18, 36);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(105, 32);
+            this.btn20.TabIndex = 5;
+            this.btn20.Text = "20₺ Öde";
+            this.btn20.UseVisualStyleBackColor = false;
+            // 
+            // btn50
+            // 
+            this.btn50.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn50.Location = new System.Drawing.Point(18, 74);
+            this.btn50.Name = "btn50";
+            this.btn50.Size = new System.Drawing.Size(105, 32);
+            this.btn50.TabIndex = 6;
+            this.btn50.Text = "50₺ Öde";
+            this.btn50.UseVisualStyleBackColor = false;
+            // 
+            // btm100
+            // 
+            this.btm100.BackColor = System.Drawing.Color.Yellow;
+            this.btm100.Location = new System.Drawing.Point(18, 112);
+            this.btm100.Name = "btm100";
+            this.btm100.Size = new System.Drawing.Size(105, 32);
+            this.btm100.TabIndex = 7;
+            this.btm100.Text = "100₺ Öde";
+            this.btm100.UseVisualStyleBackColor = false;
+            // 
+            // btn200
+            // 
+            this.btn200.BackColor = System.Drawing.Color.Fuchsia;
+            this.btn200.Location = new System.Drawing.Point(18, 150);
+            this.btn200.Name = "btn200";
+            this.btn200.Size = new System.Drawing.Size(105, 32);
+            this.btn200.TabIndex = 8;
+            this.btn200.Text = "200₺ Öde";
+            this.btn200.UseVisualStyleBackColor = false;
+            // 
+            // btn500
+            // 
+            this.btn500.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn500.Location = new System.Drawing.Point(18, 188);
+            this.btn500.Name = "btn500";
+            this.btn500.Size = new System.Drawing.Size(105, 32);
+            this.btn500.TabIndex = 9;
+            this.btn500.Text = "500₺ Öde";
+            this.btn500.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOdeme);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(16, 1);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(305, 62);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Bakiye";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 355);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkOgrenci;
+        private System.Windows.Forms.RadioButton chk4Aktarma;
+        private System.Windows.Forms.RadioButton chk3Aktarma;
+        private System.Windows.Forms.RadioButton chk2Aktarma;
+        private System.Windows.Forms.RadioButton chk1Aktarma;
+        private System.Windows.Forms.RadioButton chkİlkgiris;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn500;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn200;
+        private System.Windows.Forms.Button btn50;
+        private System.Windows.Forms.Button btm100;
+        private System.Windows.Forms.Button btnOdeme;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
